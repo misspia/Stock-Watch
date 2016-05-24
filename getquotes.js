3446b937e7e2dfdeeb52992510f096d47f781064
User <user@Users-Air.home>
2016-05-23 21:55:27 -0400

    asd

--- getquotes.js
+++ getquotes.js
@@ -1,8 +1,7 @@
-function StockPriceTicker(CNames) {
+  var StockTickerHTML = "";
+function StockPriceTicker(CNames, refresh) {
     var Symbol = "", CompName = "", Price = "", ChnageInPrice = "", PercentChnageInPrice = "";
     var flickerAPI = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + CNames + "%22)&env=store://datatables.org/alltableswithkeys";
-    var StockTickerHTML = "";
-
     var StockTickerXML = $.get(flickerAPI, function(xml) {
         $(xml).find("quote").each(function () {
             Symbol = $(this).attr("symbol");
@@ -21,21 +20,24 @@ function StockPriceTicker(CNames) {
             $(this).find("PercentChange").each(function () {
                 PercentChnageInPrice = $(this).text();
             });
+            if (!refresh) {
+              StockTickerHTML = "";
+              var PriceClass = "GreenText", PriceIcon="up_green";
+              if(parseFloat(ChnageInPrice) < 0) { PriceClass = "RedText"; PriceIcon="down_red"; }
+              StockTickerHTML = StockTickerHTML + "<div class='row'><span class='" + PriceClass + "'>";
+              StockTickerHTML = StockTickerHTML + "<div class='six columns company-view'>" + CompName + "<br><span>" + Exchange + ": " + Symbol + "</span></div> ";
 
-            var PriceClass = "GreenText", PriceIcon="up_green";
-            if(parseFloat(ChnageInPrice) < 0) { PriceClass = "RedText"; PriceIcon="down_red"; }
-            StockTickerHTML = StockTickerHTML + "<div class='row'><span class='" + PriceClass + "'>";
-            StockTickerHTML = StockTickerHTML + "<div class='six columns company-view'>" + CompName + "<br><span>" + Exchange + ": " + Symbol + "</span></div> ";
+              StockTickerHTML = StockTickerHTML + "<div class='six columns allprice-view'><div class='row'><div class='twelve columns price-view'>" + parseFloat(Price).toFixed(2) + " </div></div>";
+              StockTickerHTML = StockTickerHTML + "<div class='row'><div class='twelve columns allpchange-view'><span class='" + PriceIcon + "'></span>" + "<span class=pchange-view>" + parseFloat(Math.abs(ChnageInPrice)).toFixed(2) + " (";
+              StockTickerHTML = StockTickerHTML + parseFloat( Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%)</span></div></div></div></div>";
+            }
 
-            StockTickerHTML = StockTickerHTML + "<div class='six columns allprice-view'><div class='row'><div class='twelve columns price-view'>" + parseFloat(Price).toFixed(2) + " </div></div>";
-            StockTickerHTML = StockTickerHTML + "<div class='row'><div class='twelve columns allpchange-view'><span class='" + PriceIcon + "'></span>" + "<span class=pchange-view>" + parseFloat(Math.abs(ChnageInPrice)).toFixed(2) + " (";
-            StockTickerHTML = StockTickerHTML + parseFloat( Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%)</span></div></div></div></div>";
-
-
-             if ($("#watchtable").find('tbody #'+Symbol).length) {
-               $("tbody #"+Symbol).html("<td>"+ Symbol +"</td><td>" + CompName + "</td><td>" + Price + "</td></td><td>" + "<span class='" + PriceIcon + "'></span>" + parseFloat(Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%</td>");
-             }
-            else $("tbody").append("<tr id=" + Symbol +"><td>"+ Symbol +"</td><td>" + CompName + "</td><td>" + Price + "</td></td><td>" + "<span class='" + PriceIcon + "'></span>" + parseFloat(Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%</td></tr>");
+            if ($("#watchtable").find('tbody #'+Symbol).length) {
+              $("tbody #"+Symbol).html("<td>"+ Symbol +"</td><td>" + CompName + "</td><td>" + Price + "</td></td><td>" + "<span class='" + PriceIcon + "'></span>" + parseFloat(Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%</td>");
+            }
+            else {
+              $("tbody").append("<tr id=" + Symbol +"><td>"+ Symbol +"</td><td>" + CompName + "</td><td>" + Price + "</td></td><td>" + "<span class='" + PriceIcon + "'></span>" + parseFloat(Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%</td></tr>");
+            }
         });
 
         $("#dvStockTicker").html(StockTickerHTML);
@@ -50,7 +52,7 @@ var CNAMES = "";
 function getQuote(){
   var symbol = $("#symbol").val().toUpperCase();
   if (symbol === "") return;
-  StockPriceTicker(symbol);
+  StockPriceTicker(symbol, false);
   CNAMES += (CNAMES === "") ? symbol: "," + symbol;
 }
 $(document).ready(function(){
@@ -68,7 +70,7 @@ $(document).ready(function(){
 
     setInterval(function(){
       if (CNAMES === "") return;
-      StockPriceTicker(CNAMES);
+      StockPriceTicker(CNAMES, true);
     }, 3000);
 });
 
