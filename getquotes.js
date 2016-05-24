f3eee6f8473708a7bd1f0a6ab1ed7c7aa7587469
User <user@Users-Air.home>
2016-05-23 21:18:15 -0400

    asd

--- getquotes.js
+++ getquotes.js
@@ -30,8 +30,8 @@ function StockPriceTicker(CNames) {
             StockTickerHTML = StockTickerHTML + "<div class='six columns allprice-view'><div class='row'><div class='twelve columns price-view'>" + parseFloat(Price).toFixed(2) + " </div></div>";
             StockTickerHTML = StockTickerHTML + "<div class='row'><div class='twelve columns allpchange-view'><span class='" + PriceIcon + "'></span>" + "<span class=pchange-view>" + parseFloat(Math.abs(ChnageInPrice)).toFixed(2) + " (";
             StockTickerHTML = StockTickerHTML + parseFloat( Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%)</span></div></div></div></div>";
-        
-            
+
+
              $("#watchtable").find('tbody');
             $("tbody").append("<tr><td>"+ Symbol +"</td><td>" + CompName + "</td><td>" + Price + "</td></td><td>" + "<span class='" + PriceIcon + "'></span>" + parseFloat(Math.abs(PercentChnageInPrice.split('%')[0])).toFixed(2) + "%</td></tr>");
         });
@@ -44,13 +44,30 @@ function StockPriceTicker(CNames) {
 /*---------------------------------------------------------------------
             Submit Input
 -----------------------------------------------------------------------*/
-function getQuote(){ 
+function getQuote(){
   var symbol = $("#symbol").val().toUpperCase();
   if (symbol === "") return;
   StockPriceTicker(symbol);
   //var CNames = "^FTSE,MSFT,MSFT.SW,MSFT.MX,MSFT34.SA,MSFT.BA";
 }
+$(document).ready(function(){
+  var options = {
 
+  url: "companies.json",
+
+  getValue: "Name",
+
+  list: {
+    match: {
+      enabled: true
+    }
+  },
+
+  theme: "square"
+};
+
+  $("#symbol").easyAutocomplete(options);
+});
 
 
 //http://jsfiddle.net/chufol/Q3z9U/
@@ -59,5 +76,3 @@ function getQuote(){
 
 
 //https://jqueryui.com/autocomplete/
-
-
